740481ca339b642b126ceb1e5499353c7b48de21
Dalton Cole <dalton.cole1@gmail.com>
2026-07-11 14:08:02 -0600

    Added pages for past events

---
+++ src/_data/pastEvents.js
@@ -0,0 +1,9 @@
+const { DateTime } = require("luxon");
+const events = require("./events.json");
+
+module.exports = function () {
+  const now = DateTime.now().setZone("America/Denver").startOf("day").toMillis();
+  return events
+    .filter(e => DateTime.fromISO(e.date, { zone: "America/Denver" }).toMillis() < now)
+    .sort((a, b) => b.date.localeCompare(a.date));
+};
