5ce8416bc25ee666759b52098165b94a95547584
Dalton Cole <dalton.cole1@gmail.com>
2026-07-12 14:32:35 -0600

    Code cleanup

--- src/_data/eventCutoff.js
+++ src/_data/eventCutoff.js
@@ -1,7 +1,22 @@
 const { DateTime } = require("luxon");
+const events = require("./events.json");
 
 const GRACE_PERIOD_DAYS = 2;
 
-module.exports = function () {
+function getCutoff() {
   return DateTime.now().setZone("America/Denver").startOf("day").minus({ days: GRACE_PERIOD_DAYS }).toMillis();
+}
+
+function toMs(isoDate) {
+  return DateTime.fromISO(isoDate, { zone: "America/Denver" }).toMillis();
+}
+
+module.exports = getCutoff;
+module.exports.filterUpcoming = () => {
+  const cutoff = getCutoff();
+  return events.filter(e => toMs(e.date) >= cutoff).sort((a, b) => a.date.localeCompare(b.date));
+};
+module.exports.filterPast = () => {
+  const cutoff = getCutoff();
+  return events.filter(e => toMs(e.date) < cutoff).sort((a, b) => b.date.localeCompare(a.date));
 };
--- src/_data/pastEvents.js
+++ src/_data/pastEvents.js
@@ -1,10 +1,1 @@
-const { DateTime } = require("luxon");
-const events = require("./events.json");
-const getCutoff = require("./eventCutoff.js");
-
-module.exports = function () {
-  const cutoff = getCutoff();
-  return events
-    .filter(e => DateTime.fromISO(e.date, { zone: "America/Denver" }).toMillis() < cutoff)
-    .sort((a, b) => b.date.localeCompare(a.date));
-};
+module.exports = require("./eventCutoff.js").filterPast;
